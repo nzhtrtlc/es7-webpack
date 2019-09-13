cc5ce4280cba3d8eba078a8472ef379b709efa41
Nezih <nezih.tartilaci@gavth.com>
2019-09-13 16:13:04 +0300

    Render main react element. Updated .eslintrc to recognize react.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './style.scss'
 
-const example = () => {
-  return 'Test'
-}
-console.log('hello, world', example())
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const title = 'React with Webpack and Babel'
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+)
